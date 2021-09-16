8c13697be56534f912ec34b21a23673b9afb2bfc
DonnieZM <d.zamoram@lasallistas.org.mx>
2021-09-16 16:42:57 -0500

    Creación de VO y DAO

--- ez1/Include/Modelo/UsuariosDAO.py
+++ ez1/Include/Modelo/UsuariosDAO.py
@@ -5,3 +5,20 @@ from Include.Modelo.UsuariosVO import UsuariosVO
 class UsuariosDAO:
     def __init__(self):
         self.__tabla = "Usuarios"
+
+    def selectALL(self):
+        try:
+            conn=cnx.mysql.connect()
+            cursor=conn.cursor()
+            cursor.execute('SELECT * FROM'+self.__tabla +' ORDER BY Id DESC')
+            data=cursor.fetchall()
+            arreglo=[]
+            for fila in data:
+                uvo = UsuariosVO(fila[0], fila[1], fila[2], fila[3], fila[4], fila[5])
+                arreglo.append(uvo)
+            return arreglo
+        except Exception as d:
+            return json.dumps({'error':str(d)})
+        finally:
+            cursor.close()
+            conn.close()
