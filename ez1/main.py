bee6c88ff619d600fde9ac2e9274b33a4ad417ac
DonnieZM <d.zamoram@lasallistas.org.mx>
2021-09-19 18:43:43 -0500

    Ajuste al main.2

--- ez1/main.py
+++ ez1/main.py
@@ -3,9 +3,15 @@ from Include.Modelo.UsuariosDAO import UsuariosDAO
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<h1>Bienvenidos todos</h1>"
+@app.route("/arreglo")
+def arreglo():
+    try:
+        dao = UsuariosDAO()
+        lvo = dao.selectALL()
+        print(lvo[3].getCorreo())
+        return render_template('LoginComprador.html',eventos=lvo)
+    except Exception as e:
+        return json.dumps({'error':str(e)})
 
 if __name__ == "__main__":
     app.run()
